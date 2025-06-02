1e3df0ec83d0966bfd2c2c91c4f359309c5085aa
Risana <risanamathebula26@gmail.com>
2025-06-02 11:32:41 +0200

    Survey edit

--- Classroom_Survey.py
+++ Classroom_Survey.py
@@ -1,8 +1,55 @@
-#Samantha
-questions = [
-"Have you previously worked on a coding project? (yes/no)",
-"Should students be allowed to use phones during breaks? (yes/no)",
-"Have you finished all of your homework? (yes/no)",
-"Do you think classroom exams should be open book? (yes/no)",
-"Should homework be limited to weekends only? (yes/no)"
-]
+
+#Risana
+def tally_responses(responses):
+    tally = {}
+
+    for name, answers in responses.items():
+        for question, answer in answers.items():
+            if question not in tally:
+                tally[question] = {}
+            if answer not in tally[question]:
+                tally[question][answer] = {'count': 0, 'participants': []}
+            tally[question][answer]['count'] += 1
+            tally[question][answer]['participants'].append(name)
+
+    return tally
+
+
+if __name__ == "__main__":
+    #Samantha
+    questions = [
+    "Have you previously worked on a coding project? (yes/no)",
+    "Should students be allowed to use phones during breaks? (yes/no)",
+    "Have you finished all of your homework? (yes/no)",
+    "Do you think classroom exams should be open book? (yes/no)",
+    "Should homework be limited to weekends only? (yes/no)"
+       ]
+
+    responses = {}
+
+    # Allow multiple participants to respond
+    while True:
+        name = input("Enter your name (or type 'done' to finish): ")
+        if name.lower() == "done":
+            break
+
+        answers = {}
+        for question in questions:
+            while True:
+                answer = input(f"{question} ").strip().lower()
+                if answer in ("yes", "no"):
+                    answers[question] = answer
+                    break
+                else:
+                    #print("Please answer with 'yes' or 'no'.") Livhuwane
+        responses[name] = answers
+
+    # Tally and print the results
+    tally = tally_responses(responses)
+    #print("\n--- Tally Results ---\n") Livhuwane
+    for question, answers in tally.items():
+        print(f"Question: {question}")
+        for answer, data in answers.items():
+            participants = ', '.join(data['participants'])
+            print(f"  Answer: {answer} - Count: {data['count']} (Participants: {participants})")
+       # print() Livhuwane
