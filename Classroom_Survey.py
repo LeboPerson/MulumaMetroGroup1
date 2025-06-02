56bbb921275eaea395490a290f4703bd832da582
Lebohang <108019858+LeboPerson@users.noreply.github.com>
2025-06-02 11:37:06 +0200

    Update Classroom_Survey.py

--- Classroom_Survey.py
+++ Classroom_Survey.py
@@ -1,12 +1,4 @@
 #Lebohang's code
-# Define the survey questions
-questions = [
-"Have you previously worked on a coding project? (yes/no)",
-"Should students be allowed to use phones during breaks? (yes/no)",
-"Have you finished all of your homework? (yes/no)",
-"Do you think classroom exams should be open book? (yes/no)",
-"Should homework be limited to weekends only? (yes/no)"
-]
 # Create a list to hold all responses
 all_responses = []
 # Ask how many participants
